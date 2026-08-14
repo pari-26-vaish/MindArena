28b910dd5ca98e5a11abd3b0a909ad276aae34b6
Hemant Srivastava <hmntsriv@gmail.com>
2026-08-14 13:13:10 +0530

    MindArena V1

--- JS/quiz.js
+++ JS/quiz.js
@@ -4,21 +4,21 @@ let score = 0;
 
 // REVEAL ANIMATION CODE
 
-const reveals = document.querySelectorAll(".reveal");
+// const reveals = document.querySelectorAll(".reveal");
 
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("active");
-      }
-    });
-  },
-  {
-    threshold: 0.2,
-  },
-);
-reveals.forEach((el) => observer.observe(el));
+// const observer = new IntersectionObserver(
+//   (entries) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         entry.target.classList.add("active");
+//       }
+//     });
+//   },
+//   {
+//     threshold: 0.2,
+//   },
+// );
+// reveals.forEach((el) => observer.observe(el));
 
 // FETCH SAVED ITEMS
 const settings = JSON.parse(localStorage.getItem("quizSettings"));
--- JS/result.js
+++ JS/result.js
@@ -2,22 +2,22 @@
 // Reveal Animation
 // ==============================
 
-const reveals = document.querySelectorAll(".reveal");
+// const reveals = document.querySelectorAll(".reveal");
 
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("active");
-      }
-    });
-  },
-  {
-    threshold: 0.2,
-  },
-);
+// const observer = new IntersectionObserver(
+//   (entries) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         entry.target.classList.add("active");
+//       }
+//     });
+//   },
+//   {
+//     threshold: 0.2,
+//   },
+// );
 
-reveals.forEach((el) => observer.observe(el));
+// reveals.forEach((el) => observer.observe(el));
 
 // ==============================
 // Redirect if opened directly
